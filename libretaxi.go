891d4b682f8ba9dcbbf0e4b31710d859abedb8a0
Roman Pushkin <roman.pushkin@gmail.com>
2020-01-21 18:55:18 -0800

    Save

--- libretaxi.go
+++ libretaxi.go
@@ -1,11 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"libretaxi/config"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
 )
 
 func main() {
 	config.Init("libretaxi")
-	fmt.Printf("Using %s telegram token", config.C().Telegram_Token)
+	log.Printf("Using '%s' telegram token...\n", config.C().Telegram_Token)
+
+	bot, err := tgbotapi.NewBotAPI(config.C().Telegram_Token)
+	if err != nil {
+		log.Panic(err)
+	}
+	// bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
